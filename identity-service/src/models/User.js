2a389802ecee2fe5fd10a4ac0609f249c090a8ca
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-08-14 20:10:05 +0530

    create a logger

--- identity-service/src/models/User.js
+++ identity-service/src/models/User.js
@@ -45,5 +45,4 @@ userSchema.methods.comparePassword = async function (password) {
 userSchema.index({ username: "text" });
 
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
