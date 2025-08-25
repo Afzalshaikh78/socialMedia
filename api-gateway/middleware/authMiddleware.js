10b8c566b67c593837b6de68a33cb14cddd377eb
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-08-25 23:34:16 +0530

    Update authMiddleware.js

--- api-gateway/middleware/authMiddleware.js
+++ api-gateway/middleware/authMiddleware.js
@@ -5,17 +5,17 @@ const validateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  // 🔎 Debug logs
-  console.log("Auth header:", authHeader);
-  console.log("Extracted token:", token);
+  // // 🔎 Debug logs
+  // console.log("Auth header:", authHeader);
+  // console.log("Extracted token:", token);
 
-  if (!token) {
-    logger.warn("Access attempt without valid token!");
-    return res.status(401).json({
-      message: "Authentication required",
-      success: false,
-    });
-  }
+  // if (!token) {
+  //   logger.warn("Access attempt without valid token!");
+  //   return res.status(401).json({
+  //     message: "Authentication required",
+  //     success: false,
+  //   });
+  // }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
